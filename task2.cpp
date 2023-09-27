7b6ba4dadc31f7b5fe6ebe1b57078b88949c3c30
Thakar <vyomathakar169@gmail.com>
2023-09-27 22:30:11 +0530

    task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int a,b;
+
+
+void getdata()
+{
+    cout<<"Enter Two Number:";
+    cin>>a>>b;
+
+}
+    void cal()
+    {
+        char op;
+        cout<<"+ - * /\n";
+        cout<<"Enter Operator";
+        cin>>op;
+    
+
+    switch(op) 
+    {
+        case '+':
+            cout << "Result: " << a+b;
+            break;
+        case '-':
+            cout << "Result: " << a-b;
+            break;
+        case '*':
+            cout << "Result: " << a*b ;
+            break;
+        case '/':
+            cout << "Result: " << a/b;
+            break;
+        default:
+            cout << "Invalid Choice";
+            break;
+    }
+}
+int main()
+{
+       getdata();
+       cal();
+
+       return 0;
+}
+
+ 
